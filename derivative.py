b0052eeddd9c4ecb414d4297891b1381a0ef7724
Sebastian Mandal <sebastian.mandal@icloud.com>
2022-05-26 21:27:35 +0200

    Update derivative.py

--- derivative.py
+++ derivative.py
@@ -6,11 +6,28 @@ def derivate(*args):
     """
     res = []
     for index, arg in enumerate(args):
+        """
+        Looping through the provided parameters, figuring out the
+        exponent of the current loop item, multiplying it with
+        the value of the current item, then adding it to the
+        result array
+        """
+        
         if index == len(args) - 1:
+            """
+            If this is the last item, it's a constant, no need to
+            add it, then we break the loop
+            """
             break
+            
         res.append(float(arg) * (len(args) - (index + 1)))
+    
     return res
 
+"""
+Example input
+
+input   -> 2, 3, -1, 4 (2x^3 + 3x^2 - x + 4)
+output  -> 6, 6, -1    (6x^2 + 6x - 1)
+"""
 print(derivate(2, 3, -1, 4))
-# input   -> 2, 3, -1, 4 # 2x^3 + 3x^2 - x + 4
-# output  -> 6, 6, -1    # 6x^2 + 6x - 1
